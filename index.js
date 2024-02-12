d466bca499919c626413e4f7e4e77bb1a105e7c0
kimjuyoung99 <102804323+kimjuyoung99@users.noreply.github.com>
2024-02-12 17:44:15 +0900

    조코딩 1시간안에 끝내는 nodejs 기초 실습 코드
    조코딩 1시간안에 끝내는 nodejs 기초 실습 코드

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/sound/:name', (req, res) => {
+  const {name} = req.params
+  console.log(name)
+
+  if(name == "dog"){
+    res.json({'sound':'멍멍!!!'})
+  } else if (name == "cat"){
+    res.json({'sound':'야옹'})
+  }else if (name == "pig"){
+    res.json({'sound':'꿀꿀'})
+  }else {
+    res.json({'sound':'알수없음'})
+  }
+  
+})
+
+// app.get('/cat', (req, res) => {
+//   res.send('고양이')
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
